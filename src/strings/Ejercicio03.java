8e8c8b24514295d6b8816e529b862e261348ec23
ljbellido <ljbellido@106-18.info2>
2024-02-13 14:34:33 +0100

    3

---
+++ src/strings/Ejercicio03.java
@@ -0,0 +1,20 @@
+package strings;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int espacios(String frase) {
+		int espacios = 0;
+		
+		for(int i = 0; i < frase.length(); i++) {
+			
+		}
+		
+		return espacios;
+	}
+
+}
