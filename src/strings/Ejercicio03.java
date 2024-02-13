2f20bb8d14d65ba75cfc2ec28d15b133073191f7
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2024-02-13 19:30:18 +0100

    33

--- src/strings/Ejercicio03.java
+++ src/strings/Ejercicio03.java
@@ -1,20 +1,48 @@
 package strings;
 
+import java.util.Scanner;
+
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Variable donde guardar la frase introducida por parámetros
+		String frase = "";
+		
+		// Variable donde guardar la cantidad de espacios de una frase
+		int espacios = 0;
+		
+		// Scanner para leer del teclado
+		Scanner sc = new Scanner(System.in);
+		
+		// Pido una frase al usuario
+		System.out.println("Introduzca una frase");
+		frase = sc.nextLine();
+		
+		// Llamo a la función
+		espacios = espacios(frase);
+		
+		// Muestro la cantidad de espacios
+		System.out.println("Su frase tiene " + espacios + " espacios");
+		
+		// Cierro el Scanner
+		sc.close();
 
 	}
 	
+	// Función que devuelve los espacios de una frase introducida por parametros
 	public static int espacios(String frase) {
-		int espacios = 0;
+		//Variable para guardar los espacios
+		int numEspacios = 0;
 		
+		// Bucle que recorre el string y comprueba si tiene espacios
 		for(int i = 0; i < frase.length(); i++) {
-			
+			if(frase.charAt(i)==' ') {
+				numEspacios++;
+			}
 		}
 		
-		return espacios;
+		return numEspacios;
 	}
 
 }
